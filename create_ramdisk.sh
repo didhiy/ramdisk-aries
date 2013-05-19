cbf7d1adc479560864e40c6d5ade098426d712b4
didhiy <didhiy@gmail.com>
2013-05-20 01:13:33 +0700

    Add stock ramdisk and recovery from CM-10.1 RC-1
    unpacked using https://github.com/chirayudesai/kernel_repack

---
+++ create_ramdisk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# part of:
+# create_boot.img.sh
+#
+#
+# 2011 nubecoder
+# http://www.nubecoder.com/
+#
+echo "cleaning old file:"
+rm -f ramdisk.img
+echo "create ramdisk.img"
+mkbootfs ramdisk > ramdisk.cpio
+minigzip < ramdisk.cpio > ramdisk.img
+echo "cleaning up temp files:"
+rm -f ramdisk.cpio
+echo "move ramdisk.img to kernel directory"
+mv ramdisk.img ../android_kernel_samsung_aries/source/usr/galaxysmtd_initramfs_files/
+echo "done!"
+exit
+
