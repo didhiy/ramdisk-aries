ea2e3e3aa8ebac5e9a48d4b4a67a1dc7e3b79778
Didy Mudjeri <didhiy@gmail.com>
2013-12-16 00:00:52 +0700

    Change recovery name and destination path

--- create_ramdisk-sema-cwm.sh
+++ create_ramdisk-sema-cwm.sh
@@ -7,14 +7,14 @@
 # http://www.nubecoder.com/
 #
 echo "cleaning old file:"
-rm -f ramdisk-recovery.img
+rm -vf ramdisk-recovery.img
 echo "create ramdisk-recovery.img"
 mkbootfs ramdisk-sema-cwm > ramdisk-recovery.cpio
 minigzip < ramdisk-recovery.cpio > ramdisk-recovery.img
 echo "cleaning up temp files:"
-rm -f ramdisk-recovery.cpio
+rm -vf ramdisk-recovery.cpio
 echo "move ramdisk-recovery.img to kernel directory"
-mv ramdisk-recovery.img ../android_kernel_samsung_aries/source/usr/galaxysmtd_initramfs_files/
+mv -vf ramdisk-recovery.img ../android_kernel_samsung_aries/source/smcwm.img
 echo "done!"
 exit
 
